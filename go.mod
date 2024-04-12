3d15c8367fc76fd1bc51791f46d7cca14148e4f7
Nicolas <nicolas.galmiche@ynov.com>
2024-04-12 10:25:02 +0200

    create database and 1st test with data

--- go.mod
+++ go.mod
@@ -4,4 +4,10 @@ go 1.21.0
 
 require github.com/go-sql-driver/mysql v1.8.1
 
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	golang.org/x/net v0.17.0 // indirect
+)
 
+require github.com/mattn/go-sqlite3 v1.14.22 // indirect
--- server.go
+++ server.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
 func main() {
-	createTable()
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	gameID := 1
+
+	gameName := "Game 1"
+
+	stmt, err := db.Prepare("INSERT INTO GAMES(id, name) VALUES(?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = stmt.Exec(gameID, gameName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Values inserted successfully")
 }
